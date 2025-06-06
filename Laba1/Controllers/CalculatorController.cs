55237c6c2f3f731ccc171078029e19b4a07a3fe5
Groddo <Groddo73@mail.ru>
2025-06-06 16:13:54 +0400

    Добавьте файлы проекта.

---
+++ Laba1/Controllers/CalculatorController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laba1.Models;
+
+namespace Laba1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Numbers()
+        {
+            return View(new Opers());
+        }
+
+        [HttpPost]
+        public IActionResult Numbers(Opers opers, string action)
+        {
+            ViewBag.CheckValue = "24";
+
+            if (action == "Очистить")
+            {
+                ModelState.Clear();
+                ViewBag.CheckValue = "";
+                return View("Numbers", new Opers());
+            }
+
+            if (ModelState.IsValid)
+            {
+                switch (opers.Operation)
+                {
+                    case "+":
+                        opers.Result = opers.Oper1 + opers.Oper2;
+                        break;
+
+                    case "-":
+                        opers.Result = opers.Oper1 - opers.Oper2;
+                        break;
+
+                    case "*":
+                        opers.Result = opers.Oper1 * opers.Oper2;
+                        break;
+
+                    case "/":
+                        opers.Result = opers.Oper1 / opers.Oper2;
+                        break;
+                }
+                return View("Numbers", opers);
+
+            }
+
+    
+            return View("Numbers", opers);
+
+
+
+
+        }
+        [HttpGet]
+        public IActionResult ResultQuerry(Opers opers)
+        {
+            return View("ResultQuerry", opers);
+        }
+
+
+    }
+}
---
+++ Laba1/Models/Opers.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Laba1.Models
+{
+    public class Opers
+    {
+        [Required(ErrorMessage = "Впишите первый операнд")]
+        public int? Oper1 { get; set; }
+
+        [Required(ErrorMessage = "Впишите второй операнд")]
+        public int? Oper2 { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, выберите операцию")]
+        public string? Operation {  get; set; }
+
+        [Display(Name = "Результат вычислений")]        
+        public float? Result { get; set; }
+        
+
+    }
+}
